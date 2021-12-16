999c4e8d4368699e40aa4dec0bc7ab451284fced
Carl C <69673640+awsomesawce@users.noreply.github.com>
2021-12-16 17:24:31 -0500

    Add more classes to Classes.ps1

--- Classes.ps1
+++ Classes.ps1
@@ -27,6 +27,13 @@ class StrObjDict : System.Collections.Generic.SortedDictionary[string,object]  {
 }
 
 function Create-HyDict {
+    <#
+    .SYNOPSIS
+    Creates or adds to a HybridDictionary
+    .DESCRIPTION
+    Checks if $Key and $Val are not empty, then if a hybrid dict is supplied, add to that instead
+    of creating one
+    #>
     param (
         [ValidateNotNullOrEmpty()][string]$Key,
         [ValidateNotNull()][object]$Val,
@@ -42,3 +49,22 @@ function Create-HyDict {
     }
     return $HyDict
 }
+
+class JSONHyDict : System.Collections.Specialized.HybridDictionary {
+    [string]ToJSON() {
+        return ConvertTo-Json -InputObject $this
+    }
+}
+
+class JSONStrDict : System.Collections.Specialized.StringDictionary {
+    [string]ToJSON() {
+        <#
+        .DESCRIPTION
+        Returns a JSON array for some reason
+        #>
+        return ConvertTo-Json -InputObject $this
+    }
+}
+
+$jstrs = [JSONStrDict]::new()
+Write-Output $jstrs.Add
