723810adeabf244fdaf28723ac3a841d3ef98278
Carl C <69673640+awsomesawce@users.noreply.github.com>
2022-01-10 21:04:23 -0500

    working with [Hashtable] im pwsh

---
+++ commands.ps1
@@ -0,0 +1,29 @@
+{
+  "shells": [
+    "powershell",
+    "bash",
+    "zsh",
+    "yash: japanese made 'yet another shell'"
+  ],
+  "editors": [
+    "vim",
+    "nvim",
+    "code",
+    "kate: kde",
+    "gedit: Gnome"
+  ],
+  "consoles": [
+    "jupyter-console",
+    "jupyter-qtconsole: a gui for jupyter console",
+    "windows Terminal",
+    "the terminal software on microsoft store is good",
+    "xterm"
+  ],
+  "backupAndVersionControl": [
+    "git",
+    "tar",
+    "tarlz",
+    "bsdtar",
+    "tar-scripts: includes scripts designed to backup directories"
+  ]
+}
